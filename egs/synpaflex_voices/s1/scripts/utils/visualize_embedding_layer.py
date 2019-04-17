8a1b34fbf3a26e3705e6bea707594d70e0f1933f
asini <aghilas.sini@univ-rennes1.fr>
2019-04-17 16:35:17 +0200

    update visualization script

--- egs/synpaflex_voices/s1/scripts/utils/visualize_embedding_layer.py
+++ egs/synpaflex_voices/s1/scripts/utils/visualize_embedding_layer.py
@@ -8,7 +8,8 @@ import warnings
 import tensorflow as tf
 from tensorflow.contrib.tensorboard.plugins import projector
 
-
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 
 def build_args():
@@ -23,20 +24,42 @@ def main():
     dataFilename=build_args().dataFn[0]
     dataFrame=pandas.read_csv(dataFilename)
     dataFrame=dataFrame.fillna(0)
-    utts_epoch_id=[ str(utt) for utt in dataFrame.iloc[:,0]]
+    utts_epoch_id=[ str(utt).replace('_arctic','') for utt in dataFrame.iloc[:,0]]
     max_size=dataFrame.shape[0]
-    print(max_size)
-    utt2v = dataFrame.iloc[:,1:512].values
-    with codecs.open("./tensorboard/metadata.tsv",'w+','utf-8') as fmeta:
-        for utt in utts_epoch_id:
-            fmeta.write(utt+'\n')
-    print(type(utt2v))
+    utt2v = dataFrame.iloc[:,1:].values
+    #pretty_plot(utt2v,utts_epoch_id,out_plot_name='file_name')
+    use_tensorflow_tensorboard4project('tensorboard',utt2v,utts_epoch_id,max_size)
+
+
+def pretty_plot(data_set,data_set_labels,out_plot_name):
+    # PCA
+    pca = PCA(n_components=4)
+    data = pca.fit_transform(data_set)
+    print(type(data))
+    labels=data_set_labels
+
+    plt.subplots_adjust(bottom = 0.5)
+    plt.scatter(data[:, 0], data[:, 1], marker='o', c=data[:, 2], s=data[:, 3] * 100,cmap=plt.get_cmap('Spectral'))
+    for label, x, y in zip(labels, data[:, 0], data[:, 1]):
+        plt.annotate(
+            label,
+            xy=(x, y), xytext=(-30, 30),
+            textcoords='offset points', ha='right', va='bottom',
+            bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
+            arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0'))
+    plt.show()
+
+def use_tensorflow_tensorboard4project(log_dir_name,data_set,data_set_labels,max_size):
+    path=log_dir_name
+    with codecs.open(path+"/metadata.tsv",'w+','utf-8') as fmeta:
+        fmeta.write('utt_name\tspeaker_name\n')
+        for utt in data_set_labels:
+            speaker_name=utt.split('_')[0]
+            fmeta.write('{}\t{}\n'.format(utt,speaker_name))
     sess = tf.InteractiveSession()
     with tf.device('/cpu:0'):
-         embedding = tf.Variable(utt2v,trainable=False, name='embedding')
-    print(max_size)
+         embedding = tf.Variable(data_set,trainable=False, name='embedding')
     tf.global_variables_initializer().run()
-    path='tensorboard'
     saver = tf.train.Saver()
     writer = tf.summary.FileWriter(path,sess.graph)
     config = projector.ProjectorConfig()
